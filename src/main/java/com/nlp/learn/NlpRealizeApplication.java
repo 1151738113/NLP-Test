c3a564262f9fff2a442e5fa18fa6d78ca40e40f8
1151738113 <1151738113@qq.com>
2018-07-25 17:13:15 +0800

    first

---
+++ src/main/java/com/nlp/learn/NlpRealizeApplication.java
@@ -0,0 +1,12 @@
+package com.nlp.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlpRealizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlpRealizeApplication.class, args);
+	}
+}
